53eac98c23c197c145eccc74afb1ba53bb140c7e
staffnerluca <113279240+staffnerluca@users.noreply.github.com>
2023-10-06 06:18:35 +0200

    Create Calculations.js

---
+++ ClientApp/src/components/Calculations.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+function createRandomNums(max){
+    let first = Math.floor(Math.random()*max);
+    let second = Math.floor(Math.random()*max)
+    return (first, second)
+}
+
+function condition(){
+
+}
+
+function createMultiplicatoin(){
+
+}
+
+function createDivision(){
+
+}
+
+function createAddition(){
+
+}
+
+function createSubtraction(){
+
+}
+
+export class Calculations extends Component {
+    render(){
+        return(
+            <h1>Hello Luggi</h1>
+        )
+    }
+}
