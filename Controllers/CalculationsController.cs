df61fa3065f72ad5186e1ad863ca32315bbf92b6
Lukas Resch <lukas.resch@hak-kitz.ac.at>
2024-01-23 11:59:10 +0100

    Controller for PDF Download

--- Controllers/CalculationsController.cs
+++ Controllers/CalculationsController.cs
@@ -1,5 +1,6 @@
 using System.Data.SqlTypes;
 using System.Net.Http.Headers;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
@@ -10,20 +11,11 @@ namespace MathMaster.Controllers;
 [Route("[controller]")]
 public class CalculationController : ControllerBase
 {
-    private readonly ILogger<CalculationController> _logger;
-
-    public CalculationController(ILogger<CalculationController> logger)
-    {
-        _logger = logger;
-    }
-
-            //    "Was möchten Sie machen?\n" + "1-Multiplikation\n"
-            //+ "2-Addition\n" + "3-Division\n" + "4-Addition with numbers under each other\n" + "5-Subtraction\n"
-            //+ "6-Division with rest\n" + "7-Subtraction with numbers under each other\n" + "8-Division with numbers under each other\n";
-
     [HttpPost]
-    public PdfDocument Post(string type)
+    [Route ("api/pdf")]
+    public HttpResponseMessage GetPdf()
     {
+
         PdfDocument document = new PdfDocument();
         PdfPage page = document.AddPage();
         XGraphics gfx = XGraphics.FromPdfPage(page);
@@ -35,33 +27,34 @@ public class CalculationController : ControllerBase
         //type tells yoiu which sheet to create
         //create it
         //send it back to the user to download it
-        switch (type)
-        {
-            case "Multiplication":
-                primarySchoolTasks.MultiplicationDocument(gfx, xFont, document, headline);
-                break;
-            case "Addition":
-                primarySchoolTasks.AdditionDocument(gfx, xFont, document, headline);
-                break;
-            case "Division":
-                primarySchoolTasks.DivisonDocument(gfx, xFont, document, headline);
-                break;
-            case "Subtraction":
-                primarySchoolTasks.SubtractionDocument(gfx, xFont, document, headline);
-                break;
-            case "Addition Under":
-                primarySchoolTasks.AdditionUnderDocument(gfx, xFont, document, headline);
-                break;
-            case "Division with Rest":
-                primarySchoolTasks.DivisionWithRestDocument(gfx, xFont, document, headline);
-                break;
-            case "Subtraction Under":
-                primarySchoolTasks.SubtractionUnderDocument(gfx, xFont, document, headline);
-                break;
-            case "Division Under":
-                primarySchoolTasks.DivisionUnderDocument(gfx, xFont, document, headline);
-                break;
-        }
-        return document;
+
+        //switch (type)
+        //{
+        //    case "Multiplication":
+        //        primarySchoolTasks.MultiplicationDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Addition":
+        //        primarySchoolTasks.AdditionDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Division":
+        //        primarySchoolTasks.DivisonDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Subtraction":
+        //        primarySchoolTasks.SubtractionDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Addition Under":
+        //        primarySchoolTasks.AdditionUnderDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Division with Rest":
+        //        primarySchoolTasks.DivisionWithRestDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Subtraction Under":
+        //        primarySchoolTasks.SubtractionUnderDocument(gfx, xFont, document, headline);
+        //        break;
+        //    case "Division Under":
+        //        primarySchoolTasks.DivisionUnderDocument(gfx, xFont, document, headline);
+        //        break;
+        //}
+        return document; //PDF as response Message
     }
 }
