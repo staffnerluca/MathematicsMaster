a2d9ff4cf0801076de7c2ff5da031cfd16b130c1
Lukas Resch <lukas.resch@hak-kitz.ac.at>
2024-02-14 09:40:51 +0100

    QuestPDF

--- Controllers/CalculationsController.cs
+++ Controllers/CalculationsController.cs
@@ -29,78 +29,81 @@ public class CalculationController : ControllerBase
     public static string pathNow = Directory.GetCurrentDirectory();
 
     [HttpGet]
-    public IActionResult Get(string calculationSign)
+    public IActionResult Get(string type)
     {
-        MemoryStream stream = new MemoryStream();
-        PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
-        PdfDocument pdfDocument = primarySchoolTasks.MultiplicationDocument();
-        pdfDocument.Save(stream,false);
-        pdfDocument.Close();
-        stream.Position = 0;
-        return new FileStreamResult(stream, "application/pdf");
+        PrimarySchoolTasks_withQuestPDF primarySchoolTasks_WithQuestPDF = new PrimarySchoolTasks_withQuestPDF();
+        return primarySchoolTasks_WithQuestPDF.GenerateToMemoryStream(type);
     }
 }
 
 #region MAYBE WE NEED IT
+
+//MemoryStream stream = new MemoryStream();
+//PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
+//PdfDocument pdfDocument = primarySchoolTasks.MultiplicationDocument();
+//pdfDocument.Save(stream,false);
+//pdfDocument.Close();
+//stream.Position = 0;
+
 ////Create the necessary Files to being able to actually return a PDF Document at some point
 //PdfDocument document = new PdfDocument();
-        //XFont headline = new XFont("Arial", 32); //Prolly not working
-        //PdfPage page = document.AddPage();
-        //XGraphics gfx = XGraphics.FromPdfPage(page); //Prolly not working
-        //XFont xFont = new XFont("Arial", 20); //Prolly not working
+//XFont headline = new XFont("Arial", 32); //Prolly not working
+//PdfPage page = document.AddPage();
+//XGraphics gfx = XGraphics.FromPdfPage(page); //Prolly not working
+//XFont xFont = new XFont("Arial", 20); //Prolly not working
 
-        //PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
+//PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
 
-        //MemoryStream stream = new MemoryStream(primarySchoolTasks.CreateDocument());
-        //var stream = new FileStream(pathNow + "\\calculation.pdf", FileMode.Open); //diese Datei gibt es nicht
-        //return new FileStreamResult(stream, "application/pdf");
+//MemoryStream stream = new MemoryStream(primarySchoolTasks.CreateDocument());
+//var stream = new FileStream(pathNow + "\\calculation.pdf", FileMode.Open); //diese Datei gibt es nicht
+//return new FileStreamResult(stream, "application/pdf");
 
-        //edit page
-        //document = tasks.CreateDocument(calculationSign);
-        //return document;
+//edit page
+//document = tasks.CreateDocument(calculationSign);
+//return document;
 
-        //PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
+//PrimarySchoolTasks primarySchoolTasks = new PrimarySchoolTasks();
 
-        //#region MAYBE DELETE
-        ////new Calc
-        ////type tells you which sheet to create
-        ////create it
-        ////send it back to the user to download itIFontResolver and assign to "GlobalFontSettings.FontResolver" to use fonts."
+//#region MAYBE DELETE
+////new Calc
+////type tells you which sheet to create
+////create it
+////send it back to the user to download itIFontResolver and assign to "GlobalFontSettings.FontResolver" to use fonts."
 
-        //switch (calculationSign)
-        //{
-        //    case "Multiplication":
-        //        primarySchoolTasks.CreateDocument(calculationSign);
-        //        break;
-        //case "Addition":
-        //    primarySchoolTasks.AdditionDocument(gfx, document);
-        //    break;
-        //case "Division":
-        //    primarySchoolTasks.DivisonDocument(gfx, document);
-        //    break;
-        //case "Subtraction":
-        //    primarySchoolTasks.SubtractionDocument(gfx, document);
-        //    break;
-        //case "AdditionUnder":
-        //    primarySchoolTasks.AdditionUnderDocument(gfx, document);
-        //    break;
-        //case "DivisionWithRemainder":
-        //    primarySchoolTasks.DivisionWithRestDocument(gfx, document);
-        //    break;
-        //case "SubtractionsUnder":
-        //    primarySchoolTasks.SubtractionUnderDocument(gfx, document);
-        //    break;
-        //case "DivisionUnder":
-        //    primarySchoolTasks.DivisionUnderDocument(gfx, document);
-        //    break;
-    //}
-        //return document;
-        //return File(stream, "application/pdf", "FileDownloadName.ext");
-        //#endregion
+//switch (calculationSign)
+//{
+//    case "Multiplication":
+//        primarySchoolTasks.CreateDocument(calculationSign);
+//        break;
+//case "Addition":
+//    primarySchoolTasks.AdditionDocument(gfx, document);
+//    break;
+//case "Division":
+//    primarySchoolTasks.DivisonDocument(gfx, document);
+//    break;
+//case "Subtraction":
+//    primarySchoolTasks.SubtractionDocument(gfx, document);
+//    break;
+//case "AdditionUnder":
+//    primarySchoolTasks.AdditionUnderDocument(gfx, document);
+//    break;
+//case "DivisionWithRemainder":
+//    primarySchoolTasks.DivisionWithRestDocument(gfx, document);
+//    break;
+//case "SubtractionsUnder":
+//    primarySchoolTasks.SubtractionUnderDocument(gfx, document);
+//    break;
+//case "DivisionUnder":
+//    primarySchoolTasks.DivisionUnderDocument(gfx, document);
+//    break;
+//}
+//return document;
+//return File(stream, "application/pdf", "FileDownloadName.ext");
+//#endregion
 
-        //primarySchoolTasks.MultiplicationDocument(gfx, document);
-        //var stream = new FileStream(pathNow + "\\calculation.pdf", FileMode.Open); //diese Datei gibt es nicht
-        //return File(stream, "api/pdf", pathNow + "\\calculation.pdf"); //PDF as response Message  //Datei gibt es nicht, also PDF datei
-        //////https://stackoverflow.com/questions/40486431/return-pdf-to-the-browser-using-asp-net-core
+//primarySchoolTasks.MultiplicationDocument(gfx, document);
+//var stream = new FileStream(pathNow + "\\calculation.pdf", FileMode.Open); //diese Datei gibt es nicht
+//return File(stream, "api/pdf", pathNow + "\\calculation.pdf"); //PDF as response Message  //Datei gibt es nicht, also PDF datei
+//////https://stackoverflow.com/questions/40486431/return-pdf-to-the-browser-using-asp-net-core
 #endregion
-      
+
